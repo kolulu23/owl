7212f8e66b1706740311eace1d303bb8ecbb2418
ao.li002898 <ao.li002898@tongdun.net>
2022-02-16 15:51:56 +0800

    添加GroovyExecutor

---
+++ owl-executor-impl/src/main/java/cn/tongdun/owl/executor/OwlGroovyExecutor.java
@@ -0,0 +1,24 @@
+package cn.tongdun.owl.executor;
+
+import cn.tongdun.owl.context.OwlContext;
+
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+/**
+ * @author ao.li
+ * @date 2022/2/16 3:44 下午
+ */
+public class OwlGroovyExecutor implements OwlExecutor{
+
+
+    @Override
+    public Object execute(InputStream inputStream, Charset charset) {
+        return null;
+    }
+
+    @Override
+    public Object compile(InputStream inputStream, Charset charset) {
+        return null;
+    }
+}
