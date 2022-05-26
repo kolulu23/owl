49b042b9ca95f6474a5590c7c29aad55fe54d2d3
刘天禄 <tianlu.liu@tongdun.net>
2022-05-26 07:25:03 +0000

    Update error msg for OwlSemanticErrorEnum#DSL_DOES_NOT_RETURN

--- owl-ast/src/main/java/cn/tongdun/owl/parse/OwlSemanticErrorEnum.java
+++ owl-ast/src/main/java/cn/tongdun/owl/parse/OwlSemanticErrorEnum.java
@@ -10,7 +10,7 @@ public enum OwlSemanticErrorEnum {
     GLOBAL_VAR_NOT_INIT("Using a uninitialized global variable: "),
     VARIABLE_ASSIGN_TO_WRONG_TYPE("Cannot assign value to variable %s, required type is %s but a %s type is provided."),
     ASSIGN_TO_UNKNOWN_VARIABLE("Trying to assign a value to a undefined or uninitialized variable %s."),
-    DSL_DOES_NOT_RETURN("DSL unit must have a 'return expression' as its last statement to be executed"),
+    DSL_DOES_NOT_RETURN("DSL unit missing 'return expression' or unit evalutes null value."),
     NUMBER_TYPE_REQUIRED("This operation requires a number type, i.e. INT or DOUBLE. Found %s"),
     DIVIDE_ZERO("Dividing zero leads to undefined behaviour"),
     NON_OPERABLE_ARG_FOR_FUNCTION("None of provided args is operable for this function %s"),
